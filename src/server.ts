7e3c49d662b9818209726caf75db533b74aeb164
shahinsamiur <shahinsamiur@yahoo.com>
2026-06-27 09:40:04 +0600

    fix: mongodb and supabase config file deleted

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    // await testDBConnection();
     await testDBConnection();
     app.listen(PORT, () => {
       console.log(` Server running on port ${PORT}`);
