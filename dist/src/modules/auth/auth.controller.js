5d53fbd0ba5bc8a812490b59e24ed1fe9a340d30
shahinsamiur <shahinsamiur@yahoo.com>
2026-09-10 11:43:39 +0600

    something cors related is updated

--- dist/src/modules/auth/auth.controller.js
+++ dist/src/modules/auth/auth.controller.js
@@ -2,8 +2,8 @@ import * as authService from "./auth.service.js";
 import sendResponse from "../../shared/utils/response.js";
 const COOKIE_OPTIONS = {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 export const register = async (req, res, next) => {
